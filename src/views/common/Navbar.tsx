ae8b143f37f19219d2b02a02449646dd3f79d6e6
63OO36O PHASIT YAPRASAT <phasit.y63@rsu.ac.th>
2022-08-06 19:48:47 +0700

    init

--- src/views/common/Navbar.tsx
+++ src/views/common/Navbar.tsx
@@ -12,19 +12,17 @@ export const Navbar = () => {
 
   return (
     <div className="h-12 sm:h-14 z-20">
-      <div className="fixed top-0 w-screen h-12 sm:h-14 border-b border-gray-300 bg-blue-100 padding-20px">
+      <div className="fixed top-0 w-screen h-12 sm:h-14 border-b border-gray-300 bg-blue-100 py-10">
         <Layout className="flex w-full h-full items-center">
           <h1 className="font-bold text-blue-600">FLOW</h1>
           <div className="flex items-center flex-1 px-4 justify-center">
             <a className="mx-2 w-16" href='#home'>Home</a>
             <a className="mx-2 w-16" href='#timeline'>Timeline</a>
             <a className="mx-2 w-16" href='#blog'>Blog</a>
-            <a className="mx-2 w-16" href='#home'>Home</a>
-            <a className="mx-2 w-16" href='#timeline'>Timeline</a>
-            <a className="mx-2 w-16" href='#blog'>Blog</a>
+        
           </div>
           <div>
-            <button className="text-2xl font-bold text-white bg-blue-600 rounded-full px-12 py-4 " onClick={handleClickToRegister}>
+            <button className="CustomeRegisterNow" onClick={handleClickToRegister}>
               Register Now
             </button>
           </div>
